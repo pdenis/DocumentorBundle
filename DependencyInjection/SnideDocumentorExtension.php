6d4ffbacff7d605cfdaa5ae495e0555c6c920f56
Pascal DENIS <pascal.denis@businessdecision.com>
2014-01-25 21:00:56 +0100

    init bundle

---
+++ DependencyInjection/SnideDocumentorExtension.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the SnideDocumentor bundle.
+ *
+ * (c) Pascal DENIS <pascal.denis.75@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Snide\Bundle\DocumentorBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+/**
+ * Class SnideTravinizerExtension
+ *
+ * @author Pascal DENIS <pascal.denis.75@gmail.com>
+ */
+class SnideTravinizerExtension extends Extension
+{
+    /**
+     * Load configuration of Bundle
+     *
+     * @param array $configs Configuration parameters
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     * @throws \Exception
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+
+    }
+}
